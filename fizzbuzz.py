da385fb9259f072d731a7afe22e220a78da63a09
tatizash <98488982+tatizash@users.noreply.github.com>
2022-03-24 21:27:08 +0300

    Add files via upload

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,10 +1,11 @@
 for n in range(1, 1001):
-
-    if n % 3 == 0:
+    
+    if n % 3 == 0 and n % 5 == 0:
+        print('FizzBuzz') 
+    elif n % 3 == 0:
             print('Fizz')
     elif n % 5 == 0:
             print('Buzz')
-    elif n % 3 == 0 and n % 5 == 0:
-        print('FizzBuzz') 
+
     else:
         print(n)
