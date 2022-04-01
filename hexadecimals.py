0fea6a305ee7a6ce2ac8c80495feddb5b38e4953
tatizash <98488982+tatizash@users.noreply.github.com>
2022-04-01 20:08:19 +0300

    Add files via upload

--- hexadecimals.py
+++ hexadecimals.py
@@ -17,19 +17,27 @@ def hex_color(red, green, blue):
             (0 <= green <= 255) and
             (0 <= blue <= 255)):
 
-        hex_code = "#"
-        hex_code = hex_code + to_hex(red)
-        hex_code = hex_code + to_hex(green)
-        hex_code = hex_code + to_hex(blue)
+        hex_red=to_hex(red)
+        hex_green=to_hex(green)
+        hex_blue=to_hex(blue)
+
+        if len(hex_red)==1:
+            hex_red="0" + hex_red 
+        
+        if len(hex_green)==1:
+            hex_green="0"+hex_green
+
+        if len(hex_blue)==1:
+            hex_blue="0"+hex_blue
+
+        hex_code = "#" + hex_red + hex_green + hex_blue
         return hex_code
 
 
     else:
         return "-1"
 
-
-if __name__=='__main__':
-    red = 10
-    green = 32
-    blue = 255
-    print(hex_color(red, green, blue))
+red = 10
+green = 32
+blue = 255
+print(hex_color(red, green, blue))
