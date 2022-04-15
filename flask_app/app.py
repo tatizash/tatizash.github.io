8f1835d9f5a6c810dadd825976bf09d0cb2496e5
tatizash <98488982+tatizash@users.noreply.github.com>
2022-04-15 20:53:31 +0300

    Add files via upload

--- flask_app/app.py
+++ flask_app/app.py
@@ -39,3 +39,26 @@ def anagram (word):
             anagrams.append(key) 
 
     return render_template ('anagrams.html', word=word, is_anagram=is_anagram, anagrams=anagrams)
+
+@app.route("/dictionary/<string:prefix>")
+def search_for(prefix):
+
+    alphabet = {1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g', 8:'h', 9:'i', 10:'j', 11:'k', 12:'l', 13:'m', 14:'n', 15:'o', 16:'p', 17:'q', 18:'r', 19:'s', 20:'t', 21:'u', 22:'v', 23:'w', 24:'x', 25:'y', 26:'z'}
+    
+    strings = []
+    for key, val in alphabet.items():
+        strings.append(prefix + val)
+
+    f = open('words.txt')
+
+    word_list = f.read().splitlines()
+
+    is_real = prefix.upper() in word_list
+
+    count=0
+    for word in word_list:
+        if word.startswith(prefix.upper()):
+            count+=1
+
+
+    return render_template ('dictionary.html', prefix=prefix, strings=strings, is_real=is_real, count=count)
